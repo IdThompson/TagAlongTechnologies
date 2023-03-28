e7365103111e4e49fa18d18aa75c73689779e2e9
Idowu Thompson <idowu.thompson@decagon.dev>
2023-03-28 13:20:24 +0100

    implement the custom error messages for all the error codes and custom success message

---
+++ src/main/java/com/idowu/tagalongtechnologies/exceptions/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.idowu.tagalongtechnologies.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+    private String message;
+    private HttpStatus status;
+}
