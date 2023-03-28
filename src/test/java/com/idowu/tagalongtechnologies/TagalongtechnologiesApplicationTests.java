2239172f6bdc7a4c4aaa6abbd2f781a956b5c978
Idowu Thompson <idowu.thompson@decagon.dev>
2023-03-28 13:30:08 +0100

    Test commit

---
+++ src/test/java/com/idowu/tagalongtechnologies/TagalongtechnologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idowu.tagalongtechnologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagalongtechnologiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
