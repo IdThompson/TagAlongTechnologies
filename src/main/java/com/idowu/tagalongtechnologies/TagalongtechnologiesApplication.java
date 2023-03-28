7ffafa8190f7235ae881c957e726d374029fb70d
Idowu Thompson <idowu.thompson@decagon.dev>
2023-03-28 13:28:08 +0100

    springBootApplication

---
+++ src/main/java/com/idowu/tagalongtechnologies/TagalongtechnologiesApplication.java
@@ -0,0 +1,13 @@
+package com.idowu.tagalongtechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagalongtechnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagalongtechnologiesApplication.class, args);
+	}
+
+}
