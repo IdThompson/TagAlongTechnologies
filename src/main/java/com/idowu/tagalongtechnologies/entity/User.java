c62abc3e7eec5ab618988a3329cff01c443008e5
Idowu Thompson <idowu.thompson@decagon.dev>
2023-03-28 13:28:51 +0100

    implement the entity class

---
+++ src/main/java/com/idowu/tagalongtechnologies/entity/User.java
@@ -0,0 +1,21 @@
+package com.idowu.tagalongtechnologies.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
